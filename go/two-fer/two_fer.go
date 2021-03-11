b812a77be4222295462938cdefc7b5d20c48aa51
Manuel <manuel@velarde.me>
2021-03-11 12:28:20 -0500

    done with two_fer

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,15 +1,14 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer produces a string with the
+// name given by the user
 package twofer
 
-// ShareWith should have a comment documenting it.
+// ShareWith returns the string
+// "One for X, one for me." given
+// a string "X"
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	if name != "" {
+	return "One for " + name + ", one for me."
+	} else {
+	return "One for you, one for me."
+	}
 }
