ccb353c3a2da3ebe559ede9abe1bbd5dfbf0c4e0
Manuel <manuel@velarde.me>
2021-03-14 12:49:10 -0400

    fix error format according to go convention and add documentation

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,13 +1,17 @@
+// Package hamming calculates the hamming distance
+// between two strings
 package hamming
 
 import "errors"
 
+// Distance returns the hamming distance between the 
+// strings a and b, or an error if lengths do not match
 func Distance(a, b string) (int, error) {
 	n := len(a)
-	distance := 0
 	if n != len(b) {
-		return 0, errors.New("Length of strings do not match!")
+		return 0, errors.New("length of strings does not match")
 	}
+        distance := 0
 	for i := 0; i < n; i++ {
 		if a[i] != b[i] {
 			distance++
