7d8b6aefd718488b64211b547a5fb46d18a92b77
Manuel <manuel@velarde.me>
2021-03-14 12:53:54 -0400

    formatted file with gofmt

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -4,14 +4,14 @@ package hamming
 
 import "errors"
 
-// Distance returns the hamming distance between the 
+// Distance returns the hamming distance between the
 // strings a and b, or an error if lengths do not match
 func Distance(a, b string) (int, error) {
 	n := len(a)
 	if n != len(b) {
 		return 0, errors.New("length of strings does not match")
 	}
-        distance := 0
+	distance := 0
 	for i := 0; i < n; i++ {
 		if a[i] != b[i] {
 			distance++
