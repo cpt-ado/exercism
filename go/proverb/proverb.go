835a0c6a1a929fa824031524a5e352891ec40051
Manuel <manuel@velarde.me>
2021-03-19 12:45:34 -0400

    go formatting

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -3,14 +3,14 @@ package proverb
 
 // Proverb returns a proverb made from the given array of strings
 func Proverb(rhyme []string) []string {
-        proverb := []string{}
+	proverb := []string{}
 	length := len(rhyme)
-        if length == 0 {
-                return proverb
-        } else if length > 1 {
-                for i := 1; i < length; i++ {
-                        proverb = append(proverb, "For want of a " + rhyme[i-1] + " the " + rhyme[i] + " was lost.")
-                }
-        }
-        return append(proverb, "And all for the want of a " + rhyme[0] + ".")
+	if length == 0 {
+		return proverb
+	} else if length > 1 {
+		for i := 1; i < length; i++ {
+			proverb = append(proverb, "For want of a "+rhyme[i-1]+" the "+rhyme[i]+" was lost.")
+		}
+	}
+	return append(proverb, "And all for the want of a "+rhyme[0]+".")
 }
