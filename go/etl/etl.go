c54c8bdb21d5857c11356ea1bc722e82505f92e7
Manuel <manuel@velarde.me>
2021-03-24 02:27:34 -0400

    add documentation

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,7 +1,10 @@
+// Package etl updates old scrabble data into new format
 package etl
 
 import "strings"
 
+// Transform inverts the shape of the given map from value to slice
+// of letters into a map from letters to values
 func Transform (input map[int][]string) map[string]int {
 	output := make(map[string]int)
 	for v, letters := range input {
